5d2e61cb220ef7b6cb51a16e63c1a03cac064291
yunusemre347 <yunusemreyilmaz347@gmail.com>
2022-12-03 18:27:04 +0300

    first commit

---
+++ src/use-interval.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+//i tried something here but didnt use it. i am keeping it here for future. 
+//maybe i will copy paste this someday
+
+export function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
